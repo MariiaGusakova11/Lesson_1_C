eda4f1c571b7e2a1ce57f71cfc024fa0bdd3f1ff
Mariia <mariia.gusakova.msc@gmail.com>
2023-09-21 14:31:39 +0300

    новые расчеты

--- lesson_1_2/Program.cs
+++ lesson_1_2/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine(result);
